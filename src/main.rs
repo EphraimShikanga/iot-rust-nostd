f2f927456fd251b8c55dc94583fe89eccef6dc51
EphraimShikanga <ephraimshikanga@gmail.com>
2024-08-26 17:16:01 +0300

    feat: Migrate to embassy_time and esp_hal_embassy
    This commit replaces the polling-based delay mechanism with embassy_time and esp_hal_embassy
    for asynchronous operations, enhancing the efficiency and responsiveness of the code.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 #![no_std]
 #![no_main]
 
+extern crate alloc;
+#[allow(unused_imports)]
 use esp_backtrace as _;
+
+use embassy_time::{Duration, Timer};
 use esp_hal::{
-    clock::ClockControl, delay::Delay, peripherals::Peripherals, prelude::*, system::SystemControl,
+    clock::ClockControl,
+    peripherals::Peripherals,
+    prelude::*,
+    rng::Rng,
+    system::SystemControl,
+    timer::{ErasedTimer, OneShotTimer, PeriodicTimer}
 };
 
-extern crate alloc;
 use core::mem::MaybeUninit;
 
 #[global_allocator]
@@ -21,33 +29,60 @@ fn init_heap() {
     }
 }
 
-#[entry]
-fn main() -> ! {
+// When you are okay with using a nightly compiler it's better to use https://docs.rs/static_cell/2.1.0/static_cell/macro.make_static.html
+macro_rules! mk_static {
+    ($t:ty,$val:expr) => {{
+        static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
+        #[deny(unused_attributes)]
+        let x = STATIC_CELL.uninit().write(($val));
+        x
+    }};
+}
+
+#[embassy_executor::task]
+async fn task() {
+    esp_println::println!("bowing!");
+    Timer::after(Duration::from_millis(1_000)).await;
+}
+
+#[main]
+async fn main(spawner: embassy_executor::Spawner) -> ! {
+    esp_println::logger::init_logger_from_env();
+
     let peripherals = Peripherals::take();
     let system = SystemControl::new(peripherals.SYSTEM);
 
     let clocks = ClockControl::max(system.clock_control).freeze();
-    let delay = Delay::new(&clocks);
     init_heap();
 
-    esp_println::logger::init_logger_from_env();
 
-    let timer = esp_hal::timer::PeriodicTimer::new(
-        esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG1, &clocks, None)
+    let timer = PeriodicTimer::new(
+        esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG0, &clocks, None)
             .timer0
             .into(),
     );
     let _init = esp_wifi::initialize(
         esp_wifi::EspWifiInitFor::Wifi,
         timer,
-        esp_hal::rng::Rng::new(peripherals.RNG),
+        Rng::new(peripherals.RNG),
         peripherals.RADIO_CLK,
         &clocks,
     )
     .unwrap();
 
+    let timg1 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG1, &clocks, None);
+    esp_hal_embassy::init(
+        &clocks,
+        mk_static!(
+            [OneShotTimer<ErasedTimer>; 1],
+            [OneShotTimer::new(timg1.timer0.into())]
+        ),
+    );
+
+    spawner.spawn(task()).ok();
+
     loop {
-        log::info!("Hello world!");
-        delay.delay(500.millis());
+        esp_println::println!("Bing!");
+        Timer::after(Duration::from_millis(5_000)).await;
     }
 }
